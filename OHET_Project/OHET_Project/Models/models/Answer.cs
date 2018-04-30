d248c8288750dd42369b8fbd7d161200ebdc8616
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 12:38:37 +0200

    Answer model filled

--- OHET_Project/OHET_Project/Models/models/Answer.cs
+++ OHET_Project/OHET_Project/Models/models/Answer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,20 @@ namespace OHET_Project.Models.models
 {
     public class Answer
     {
+        [Key]
+        public int IDAnswer { get; set; }
+
+        [Required]
+        public string choice { get; set; }
+
+        public string comment { get; set; }
+
+        [ForeignKey("IDPoll")]
+        public Poll Poll { get; set; }
+        public int? IDPoll { get; set; }
+
+        [ForeignKey("IDUser")]
+        public User User { get; set; }
+        public int? IDUser { get; set; }
     }
 }
