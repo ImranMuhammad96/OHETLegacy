58304cb69ae9cadf6e4a3ab45b12a5bcddf04250
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 15:40:40 +0200

    Ability model filled

--- OHET_Project/OHET_Project/Models/models/Ability.cs
+++ OHET_Project/OHET_Project/Models/models/Ability.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,20 @@ namespace OHET_Project.Models.models
 {
     public class Ability
     {
+        [Key]
+        public int IDPoll { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public string description { get; set; }
+
+        [Required]
+        public int conceptLvl { get; set; }
+
+        [ForeignKey("IDClass")]
+        public Class Class { get; set; }
+        public int? IDClass { get; set; }
     }
 }
