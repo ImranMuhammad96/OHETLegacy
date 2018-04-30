fa64de1bfb4154f8c1e59aa7fd1f85ae7b8d2143
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 12:33:40 +0200

    Poll model filled

--- OHET_Project/OHET_Project/Models/models/Poll.cs
+++ OHET_Project/OHET_Project/Models/models/Poll.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,20 @@ namespace OHET_Project.Models.models
 {
     public class Poll
     {
+        [Key]
+        public int IDPoll { get; set; }
+
+        [ForeignKey("IDUser")]
+        public User User { get; set; }
+        public int? IDUser { get; set; }
+
+        [Required]
+        public string question { get; set; }
+
+        public string description { get; set; }
+
+        public ICollection<Poll> Polls { get; set; }
+        public ICollection<Answer> Answers { get; set; }
+        public ICollection<Content> Contents { get; set; }
     }
 }
