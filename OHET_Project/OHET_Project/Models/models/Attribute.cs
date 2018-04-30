3ffc75cc1be1864a5020b1b2e174f5197a50caec
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 15:20:18 +0200

    Attribute model filled

--- OHET_Project/OHET_Project/Models/models/Attribute.cs
+++ OHET_Project/OHET_Project/Models/models/Attribute.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,15 @@ namespace OHET_Project.Models.models
 {
     public class Attribute
     {
+        [Key]
+        public int IDAttribute { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public int level { get; set; }
+
+        public ICollection<HA> HA { get; set; }
     }
 }
