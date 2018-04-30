584d23aa7a8e9fcfe52418ea822ca2ecbf059da0
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 15:29:45 +0200

    Armor model filled

--- OHET_Project/OHET_Project/Models/models/Armor.cs
+++ OHET_Project/OHET_Project/Models/models/Armor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,18 @@ namespace OHET_Project.Models.models
 {
     public class Armor
     {
+        [Key]
+        [ForeignKey("IDItem")]
+        public Item Item { get; set; }
+        public int? IDItem { get; set; }
+
+        [ForeignKey("IDClass")]
+        public Class Class { get; set; }
+        public int? IDClass { get; set; }
+
+        [Required]
+        public int armorClass { get; set; }
+
+        public ICollection<CA> CA { get; set; }
     }
 }
