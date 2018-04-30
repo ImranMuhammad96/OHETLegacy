83e0623dfcb10f6688f509b5b45874962b9e7a97
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 15:08:56 +0200

    Hero model filled

--- OHET_Project/OHET_Project/Models/models/Hero.cs
+++ OHET_Project/OHET_Project/Models/models/Hero.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,32 @@ namespace OHET_Project.Models.models
 {
     public class Hero
     {
+        [Key]
+        public int IDHero { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public int armorClassSum { get; set; }
+
+        [Required]
+        public int gold { get; set; }
+
+        [Required]
+        public int exp { get; set; }
+
+        [ForeignKey("IDConcept")]
+        public Concept Concept { get; set; }
+        public int? IDConcept { get; set; }
+
+        [ForeignKey("IDContent")]
+        public Content Content { get; set; }
+        public int? IDContent { get; set; }
+
+        public ICollection<Equipment> Equipment { get; set; }
+        public ICollection<HA> HA { get; set; }
+        public ICollection<HL> HL { get; set; }
+        public ICollection<HC> HC { get; set; }
     }
 }
