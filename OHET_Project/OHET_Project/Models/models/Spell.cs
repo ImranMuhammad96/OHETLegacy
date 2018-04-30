3af9776b21c9c790571f3c429d37162ea80a9ee5
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 15:15:23 +0200

    Spell model filled

--- OHET_Project/OHET_Project/Models/models/Spell.cs
+++ OHET_Project/OHET_Project/Models/models/Spell.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,20 @@ namespace OHET_Project.Models.models
 {
     public class Spell
     {
+        [Key]
+        public int IDSpell { get; set; }
+
+        [Required]
+        public string description { get; set; }
+
+        [ForeignKey("IDConcept")]
+        public Concept Concept { get; set; }
+        public int? IDConcept { get; set; }
+
+        [ForeignKey("IDContent")]
+        public Content Content { get; set; }
+        public int? IDContent { get; set; }
+
+        public ICollection<SS> SS { get; set; }
     }
 }
