a914f2b77251c3085b79aab8691411964294c28c
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 15:25:15 +0200

    SS model filled

--- OHET_Project/OHET_Project/Models/models/SS.cs
+++ OHET_Project/OHET_Project/Models/models/SS.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,14 @@ namespace OHET_Project.Models.models
 {
     public class SS
     {
+        [Key]
+        [ForeignKey("IDSpell")]
+        public Spell Spell { get; set; }
+        public int? IDSpell { get; set; }
+
+        [Key]
+        [ForeignKey("IDClass")]
+        public Class Class { get; set; }
+        public int? IDClass { get; set; }
     }
 }
