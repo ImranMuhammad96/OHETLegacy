ea2f6f6be213574d79d368dcadd5defba46b6c47
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 12:55:03 +0200

    Monster model filled

--- OHET_Project/OHET_Project/Models/models/Monster.cs
+++ OHET_Project/OHET_Project/Models/models/Monster.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,21 @@ namespace OHET_Project.Models.models
 {
     public class Monster
     {
+        [Key]
+        public int IDMonster { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public string description { get; set; }
+
+        [ForeignKey("IDConcept")]
+        public Concept Concept { get; set; }
+        public int? IDConcept { get; set; }
+
+        [ForeignKey("IDContent")]
+        public Content Content { get; set; }
+        public int? IDContent { get; set; }
     }
 }
