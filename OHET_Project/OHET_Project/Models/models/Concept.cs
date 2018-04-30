801f61564fc807bb0730730005f994b0cfcf8229
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 15:11:51 +0200

    Concept model filled

--- OHET_Project/OHET_Project/Models/models/Concept.cs
+++ OHET_Project/OHET_Project/Models/models/Concept.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,16 @@ namespace OHET_Project.Models.models
 {
     public class Concept
     {
+        [Key]
+        public int IDConcept { get; set; }
+
+        [Required]
+        public int level { get; set; }
+
+        public string description { get; set; }
+
+        public ICollection<Hero> Hero { get; set; }
+        public ICollection<Monster> Monster { get; set; }
+        public ICollection<Spell> Spell { get; set; }
     }
 }
