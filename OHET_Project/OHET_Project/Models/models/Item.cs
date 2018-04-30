8f9b18c1e7de48fc731463878cf7ffe8a14c9a42
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 15:18:06 +0200

    Item model filled

--- OHET_Project/OHET_Project/Models/models/Item.cs
+++ OHET_Project/OHET_Project/Models/models/Item.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,22 @@ namespace OHET_Project.Models.models
 {
     public class Item
     {
+        [Key]
+        public int IDItem { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public int cost { get; set; }
+
+        [Required]
+        public string description { get; set; }
+
+        [ForeignKey("IDContent")]
+        public Content Content { get; set; }
+        public int? IDContent { get; set; }
+
+        public ICollection<EI> EI { get; set; }
     }
 }
