54b265708db8b56f38055178e3fb766e429b9fb3
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 12:26:30 +0200

    User model filled

--- OHET_Project/OHET_Project/Models/models/User.cs
+++ OHET_Project/OHET_Project/Models/models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,17 @@ namespace OHET_Project.Models.models
 {
     public class User
     {
+        [Key]
+        public int IDUser { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public string email { get; set; }
+
+        public ICollection<Poll> Polls { get; set; }
+        public ICollection<Answer> Answers { get; set; }
+        public ICollection<Content> Contents { get; set; }
     }
 }
