d1abac951fd9341252002d262bc0fdb22af257b9
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 12:48:53 +0200

    Content model filled

--- OHET_Project/OHET_Project/Models/models/Content.cs
+++ OHET_Project/OHET_Project/Models/models/Content.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,25 @@ namespace OHET_Project.Models.models
 {
     public class Content
     {
+        [Key]
+        public int IDContent { get; set; }
+
+        [ForeignKey("IDUser")]
+        public User User { get; set; }
+        public int? IDUser { get; set; }
+
+        [Required]
+        public bool isOfficial { get; set; }
+
+        [Required]
+        public bool isPublic { get; set; }
+
+        public ICollection<Monster> Monsters { get; set; }
+        public ICollection<Class> Classes { get; set; }
+        public ICollection<Hero> Heros { get; set; }
+        public ICollection<Spell> Spells { get; set; }
+        public ICollection<Item> Items { get; set; }
+        public ICollection<Rule> Rules { get; set; }
+        public ICollection<Adventure> Adventures { get; set; }
     }
 }
