fb4a380cb7a99502691bc2248dc198178e0017db
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 15:47:01 +0200

    Fistcaster model filled

--- OHET_Project/OHET_Project/Models/models/Fistcaster.cs
+++ OHET_Project/OHET_Project/Models/models/Fistcaster.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,9 @@ namespace OHET_Project.Models.models
 {
     public class Fistcaster
     {
+        [Key]
+        [ForeignKey("IDClass")]
+        public Class Class { get; set; }
+        public int? IDClass { get; set; }
     }
 }
