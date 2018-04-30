43de8991b49ccbc6613b69e68676ae7f236cb1c4
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 14:57:31 +0200

    Equipment model filled

--- OHET_Project/OHET_Project/Models/models/Equipment.cs
+++ OHET_Project/OHET_Project/Models/models/Equipment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,13 @@ namespace OHET_Project.Models.models
 {
     public class Equipment
     {
+        [Key]
+        public int IDEquipment { get; set; }
+
+        [ForeignKey("IDHero")]
+        public Hero Hero { get; set; }
+        public int? IDHero { get; set; }
+
+        public ICollection<EI> EI { get; set; }
     }
 }
