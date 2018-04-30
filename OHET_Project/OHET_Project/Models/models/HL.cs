df81e669dd3eba7646b0dab2bcc6f60705a8a0e2
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 15:22:00 +0200

    HL model filled

--- OHET_Project/OHET_Project/Models/models/HL.cs
+++ OHET_Project/OHET_Project/Models/models/HL.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,14 @@ namespace OHET_Project.Models.models
 {
     public class HL
     {
+        [Key]
+        [ForeignKey("IDHero")]
+        public Hero Hero { get; set; }
+        public int? IDHero { get; set; }
+
+        [Key]
+        [ForeignKey("IDLabel")]
+        public Label Label { get; set; }
+        public int? IDLabel { get; set; }
     }
 }
