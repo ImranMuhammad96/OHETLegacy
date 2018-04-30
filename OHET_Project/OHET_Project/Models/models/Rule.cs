2f3c08e3936aee99e04d18cd595b1bd4440563c1
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 12:51:53 +0200

    Rule model filled

--- OHET_Project/OHET_Project/Models/models/Rule.cs
+++ OHET_Project/OHET_Project/Models/models/Rule.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,17 @@ namespace OHET_Project.Models.models
 {
     public class Rule
     {
+        [Key]
+        public int IDRule { get; set; }
+
+        [Required]
+        public string title { get; set; }
+
+        [Required]
+        public string description { get; set; }
+
+        [ForeignKey("IDContent")]
+        public Content Content { get; set; }
+        public int? IDContent { get; set; }
     }
 }
