c201f3045391f922135e87d8c55d28537d20beb6
Imran Muhammad <s13809@pjwstk.edu.pl>
2018-04-30 15:28:07 +0200

    Weapon model filled

--- OHET_Project/OHET_Project/Models/models/Weapon.cs
+++ OHET_Project/OHET_Project/Models/models/Weapon.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,14 @@ namespace OHET_Project.Models.models
 {
     public class Weapon
     {
+        [Key]
+        [ForeignKey("IDItem")]
+        public Item Item { get; set; }
+        public int? IDItem { get; set; }
+
+        [Required]
+        public string type { get; set; }
+
+        public ICollection<CW> CW { get; set; }
     }
 }
