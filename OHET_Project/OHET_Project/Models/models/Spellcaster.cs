d2d6177c98f9380f981aa2730d4efa8ea909b7c1
Marcin Krupa <marcinkgm@gmail.com>
2018-05-25 16:09:47 +0200

    many to many in spell-spellcaster corrected

--- OHET_Project/OHET_Project/Models/models/Spellcaster.cs
+++ OHET_Project/OHET_Project/Models/models/Spellcaster.cs
@@ -9,9 +9,17 @@ namespace OHET_Project.Models.models
 {
     public class Spellcaster
     {
+
+        public Spellcaster()
+        {
+            this.spells = new HashSet<Spell>();
+        }
+
         [Key]
         [ForeignKey("IDClass")]
         public Class Class { get; set; }
         public int? IDClass { get; set; }
+
+        public virtual ICollection<Spell> spells { get; set; }
     }
 }
