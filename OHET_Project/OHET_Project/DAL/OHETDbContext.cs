25d1ded593a4b725fa5947a7e256f1109dd28a50
Marcin Krupa <s13711@pjwstk.edu.pl>
2018-05-28 17:31:45 +0200

    some of getters and setters added

---
+++ OHET_Project/OHET_Project/DAL/OHETDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OHET_Project.DAL
+{
+    public class OHETDbContext : DbContext
+    {
+
+        public OHETDbContext()
+            : base("DefaultConnection")
+        {
+            
+        }
+
+        public DbSet<Ability> Abilities { get; set; }
+        public DbSet<Adventure> Adventures { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<Armor> Armors { get; set; }
+
+    }
+}
