e31c55fc7f10c35857b7f11ba145c67172d6cf82
Marcin Krupa <s13711@pjwstk.edu.pl>
2018-11-14 20:45:11 +0100

    Character-Sheet-Fix: Added using library (Diagnostics) to models

--- OHET_Project/OHET_Project/Models/models/Rule.cs
+++ OHET_Project/OHET_Project/Models/models/Rule.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
