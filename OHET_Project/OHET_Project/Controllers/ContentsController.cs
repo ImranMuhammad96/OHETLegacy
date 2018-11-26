191432fd1678fef0826ce4514c5ded9a21837542
s13809 <s13809@pjwstk.edu.pl>
2018-11-26 18:38:50 +0100

    Fixed stuff.

---
+++ OHET_Project/OHET_Project/Controllers/ContentsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OHET_Project.Models.models;
+using OHET_Project.Persistence;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace OHET_Project.Controllers
+{
+    public class ContentsController : Controller
+    {
+        private Persistence.DbContext db = new Persistence.DbContext();
+
+        // GET: Contents
+        public ActionResult Index()
+        {
+            ViewBag.userId = User.Identity.GetUserId();
+
+            var contents = db.contents.Include(c => c.ApplicationUser);
+            return View(contents.ToList());
+        }
+
+        // GET: Contents/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Content content = db.contents.Find(id);
+            if (content == null)
+            {
+                return HttpNotFound();
+            }
+            return View(content);
+        }
+
+        // GET: Contents/Create
+        public ActionResult Create()
+        {
+            ViewBag.ApplicationUserId = new SelectList(db.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Contents/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDContent,isOfficial,isPublic,ApplicationUserId")] Content content)
+        {
+            if (ModelState.IsValid)
+            {
+                db.contents.Add(content);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ApplicationUserId = new SelectList(db.Users, "Id", "Email", content.ApplicationUserId);
+            return View(content);
+        }
+
+        // GET: Contents/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Content content = db.contents.Find(id);
+            if (content == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ApplicationUserId = new SelectList(db.Users, "Id", "Email", content.ApplicationUserId);
+            return View(content);
+        }
+
+        // POST: Contents/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDContent,isOfficial,isPublic,ApplicationUserId")] Content content)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(content).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ApplicationUserId = new SelectList(db.Users, "Id", "Email", content.ApplicationUserId);
+            return View(content);
+        }
+
+        // GET: Contents/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Content content = db.contents.Find(id);
+            if (content == null)
+            {
+                return HttpNotFound();
+            }
+            return View(content);
+        }
+
+        // POST: Contents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Content content = db.contents.Find(id);
+            db.contents.Remove(content);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- OHET_Project/OHET_Project/Migrations/201811261703209_Init.Designer.cs
+++ OHET_Project/OHET_Project/Migrations/201811261703209_Init.Designer.cs
@@ -7,13 +7,13 @@ namespace OHET_Project.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class Ini : IMigrationMetadata
+    public sealed partial class Init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Ini));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201811141054440_Ini"; }
+            get { return "201811261703209_Init"; }
         }
         
         string IMigrationMetadata.Source
