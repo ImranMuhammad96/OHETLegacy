0fa6b99fef8040ddc59260c4874b60f8eba9e514
Imran Muhammad <s13809@pjwstk.edu.pl>
2019-01-12 22:21:43 +0100

    Fixed descriptions and navbar.

--- OHET_Project/OHET_Project/Controllers/HomeController.cs
+++ OHET_Project/OHET_Project/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace OHET_Project.Controllers
             return View();
         }
 
+        public ActionResult Content()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
